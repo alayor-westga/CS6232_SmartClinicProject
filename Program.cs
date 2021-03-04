3321dd326729d50407aed5baae7dd359c01af521
Alonso Ortega <aayalao1@my.westga.edu>
2021-03-04 09:43:02 -0700

    Add windows forms project and solution.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SmartClinic
+{
+    /// <summary>
+    /// The main class for the application.
+    /// </summary>
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            try 
+            {
+                if (Environment.OSVersion.Version.Major >= 6)
+                    SetProcessDPIAware();
+            }
+            catch(Exception e) 
+            {
+                Console.WriteLine(e.Message);
+                // ignore
+            }
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new View.LoginForm());
+        }
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern bool SetProcessDPIAware();
+    }
+}
