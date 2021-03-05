a6918d28cfea2307ab19b2f30ee8d6fb35e27b2b
Alonso Ortega <aayalao1@my.westga.edu>
2021-03-04 17:55:02 -0700

    Add UI controls to the Login Form.

--- View/LoginForm.Designer.cs
+++ View/LoginForm.Designer.cs
@@ -29,20 +29,116 @@ namespace SmartClinic.View
         /// </summary>
         private void InitializeComponent()
         {
+            this.smartLabel = new System.Windows.Forms.Label();
+            this.usernameTextBox = new System.Windows.Forms.TextBox();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.usernameLabel = new System.Windows.Forms.Label();
+            this.passwordLabel = new System.Windows.Forms.Label();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.clinicLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // smartLabel
+            // 
+            this.smartLabel.AutoSize = true;
+            this.smartLabel.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.smartLabel.Font = new System.Drawing.Font("Consolas", 13.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.smartLabel.Location = new System.Drawing.Point(242, 33);
+            this.smartLabel.Name = "smartLabel";
+            this.smartLabel.Size = new System.Drawing.Size(119, 43);
+            this.smartLabel.TabIndex = 0;
+            this.smartLabel.Text = "smart";
+            this.smartLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // usernameTextBox
+            // 
+            this.usernameTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.usernameTextBox.Location = new System.Drawing.Point(279, 110);
+            this.usernameTextBox.Name = "usernameTextBox";
+            this.usernameTextBox.Size = new System.Drawing.Size(342, 49);
+            this.usernameTextBox.TabIndex = 1;
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.passwordTextBox.Location = new System.Drawing.Point(279, 187);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.Size = new System.Drawing.Size(342, 49);
+            this.passwordTextBox.TabIndex = 2;
+            // 
+            // usernameLabel
+            // 
+            this.usernameLabel.AutoSize = true;
+            this.usernameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.usernameLabel.Location = new System.Drawing.Point(89, 113);
+            this.usernameLabel.Name = "usernameLabel";
+            this.usernameLabel.Size = new System.Drawing.Size(184, 42);
+            this.usernameLabel.TabIndex = 3;
+            this.usernameLabel.Text = "username";
+            // 
+            // passwordLabel
+            // 
+            this.passwordLabel.AutoSize = true;
+            this.passwordLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.passwordLabel.Location = new System.Drawing.Point(89, 190);
+            this.passwordLabel.Name = "passwordLabel";
+            this.passwordLabel.Size = new System.Drawing.Size(179, 42);
+            this.passwordLabel.TabIndex = 4;
+            this.passwordLabel.Text = "password";
+            // 
+            // loginButton
+            // 
+            this.loginButton.AutoSize = true;
+            this.loginButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.loginButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loginButton.Location = new System.Drawing.Point(364, 266);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(163, 52);
+            this.loginButton.TabIndex = 3;
+            this.loginButton.Text = "Login";
+            this.loginButton.UseVisualStyleBackColor = true;
+            // 
+            // clinicLabel
+            // 
+            this.clinicLabel.AutoSize = true;
+            this.clinicLabel.Font = new System.Drawing.Font("Consolas", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clinicLabel.Location = new System.Drawing.Point(347, 8);
+            this.clinicLabel.Name = "clinicLabel";
+            this.clinicLabel.Size = new System.Drawing.Size(242, 75);
+            this.clinicLabel.TabIndex = 6;
+            this.clinicLabel.Text = "Clinic";
             // 
             // LoginForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 352);
+            this.Controls.Add(this.loginButton);
+            this.Controls.Add(this.passwordLabel);
+            this.Controls.Add(this.usernameLabel);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.usernameTextBox);
+            this.Controls.Add(this.smartLabel);
+            this.Controls.Add(this.clinicLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "LoginForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "LoginForm";
+            this.Text = "Login";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label smartLabel;
+        private System.Windows.Forms.TextBox usernameTextBox;
+        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.Label usernameLabel;
+        private System.Windows.Forms.Label passwordLabel;
+        private System.Windows.Forms.Button loginButton;
+        private System.Windows.Forms.Label clinicLabel;
     }
 }
