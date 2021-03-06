8c9eb350ab256e1c0df5ded5a8f364cdc36f6b86
Alonso Ortega <aayalao1@my.westga.edu>
2021-03-06 07:19:05 -0700

    Add missing comments in the ClinicPerson class.

--- Model/ClinicPerson.cs
+++ Model/ClinicPerson.cs
@@ -17,15 +17,44 @@ namespace SmartClinic.Model
         /// </summary>
         public string LastName { get; set; }
 
+        /// <summary>
+        /// The date of birth of the clinic person.
+        /// </summary>
         public DateTime DateOfBirth { get; set; }
+        
+        /// <summary>
+        /// The gender of the clinic person.
+        /// </summary>
         public string Gender { get; set; }
+
+        /// <summary>
+        /// The street 1 of the clinic person.
+        /// </summary>
         public string Street1 { get; set; }
+
+        /// <summary>
+        /// The street 2 of the clinic person.
+        /// </summary>
         public string Street2 { get; set; }
+        
+        /// <summary>
+        /// The city of the clinic person.
+        /// </summary>
         public string City { get; set; }
+        
+        /// <summary>
+        /// The state of the clinic person.
+        /// </summary>
         public string State { get; set; }
+        
+        /// <summary>
+        /// The zip code of the clinic person.
+        /// </summary>
         public int ZipCode { get; set; }
+        
+        /// <summary>
+        /// The phone of the clinic person.
+        /// </summary>
         public int Phone { get; set; }
-
-
     }
 }
