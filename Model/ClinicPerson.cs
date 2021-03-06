ac53de1d8ef84cd0c406e5fbc91147d4b7614e87
Mike Hamel <mhamel1@my.westga.edu>
2021-03-06 01:26:13 -0800

    Revert "Revert "added remaining fields to ClinicPerson model""
    This reverts commit 997ad47eaecf62ad1bdc7ce19e163268ad6c223e.

--- Model/ClinicPerson.cs
+++ Model/ClinicPerson.cs
@@ -16,5 +16,16 @@ namespace SmartClinic.Model
         /// The last name of the clinic person.
         /// </summary>
         public string LastName { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+        public string Gender { get; set; }
+        public string Street1 { get; set; }
+        public string Street2 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int ZipCode { get; set; }
+        public int Phone { get; set; }
+
+
     }
 }
