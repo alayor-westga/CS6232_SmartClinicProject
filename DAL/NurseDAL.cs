cdc52d614b98edc3b07ce878064595f0b6d91d75
Mike Hamel <mhamel1@my.westga.edu>
2021-03-06 01:24:54 -0800

    Revert "Revert "Add the Patient Model and DAL classes.""
    This reverts commit 59c7c78c3867a8bc3f4cad431a06fdb4c119ad42.

--- DAL/NurseDAL.cs
+++ DAL/NurseDAL.cs
@@ -13,6 +13,8 @@ namespace SmartClinic.DAL
         /// <summary>
         /// It verifies if a nurse exists with the provided username and password.
         /// </summary>
+        /// <param name="username">The nurse's username.</param>
+        /// <param name="password">The nurse's password.</param>
         /// <returns>The nurse if found. Null otherwise.</returns>
         public Nurse GetByUserNameAndPassword(string username, string password)
         {
---
+++ Model/Patient.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SmartClinic.Model
+{
+    /// <summary>
+    /// The Patient model class.
+    /// </summary>
+    public class Patient : ClinicPerson
+    {
+        /// <summary>
+        /// The Id of the Patient.
+        /// </summary>
+        public int PatientId { get; set; }
+    }
+}
