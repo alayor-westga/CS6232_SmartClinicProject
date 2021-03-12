61d0610c983ae2a3aabd2aff53f8752987832bb0
Mike Hamel <mhamel1@my.westga.edu>
2021-03-11 22:28:03 -0800

    created PatientVisitController

---
+++ Controller/PatientVisitController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using SmartClinic.DAL;
+using SmartClinic.Model;
+
+namespace SmartClinic.Controller
+{
+    /// <summary>
+    /// Middleware between the AppointmentDAL and the View.
+    /// </summary>
+    public class PatientVisitController
+    {
+        private readonly AppointmentDAL appointmentSource;
+        private readonly VisitDAL visitSource;
+
+        /// <summary>
+        /// It creates a AppointmentController object.
+        /// </summary>
+        public PatientVisitController()
+        {
+        }
+    }
+}
