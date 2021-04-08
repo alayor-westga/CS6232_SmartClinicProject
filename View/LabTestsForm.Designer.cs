1e9555a972331f162bbd735a876aa0844a154123
Alonso Ortega <aayalao1@my.westga.edu>
2021-04-07 23:39:32 -0600

    Render the lab tests in the combobox.

--- View/LabTestsForm.Designer.cs
+++ View/LabTestsForm.Designer.cs
@@ -29,18 +29,24 @@ namespace SmartClinic.View
         /// </summary>
         private void InitializeComponent()
         {
-            this.testsComboBox = new System.Windows.Forms.ComboBox();
+            this.components = new System.ComponentModel.Container();
+            this.labTestsComboBox = new System.Windows.Forms.ComboBox();
             this.chooseATestLabel = new System.Windows.Forms.Label();
+            this.labTestBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.labTestBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
-            // testsComboBox
+            // labTestsComboBox
             // 
-            this.testsComboBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.testsComboBox.FormattingEnabled = true;
-            this.testsComboBox.Location = new System.Drawing.Point(291, 153);
-            this.testsComboBox.Name = "testsComboBox";
-            this.testsComboBox.Size = new System.Drawing.Size(475, 45);
-            this.testsComboBox.TabIndex = 0;
+            this.labTestsComboBox.DataSource = this.labTestBindingSource;
+            this.labTestsComboBox.DisplayMember = "Name";
+            this.labTestsComboBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labTestsComboBox.FormattingEnabled = true;
+            this.labTestsComboBox.Location = new System.Drawing.Point(291, 153);
+            this.labTestsComboBox.Name = "labTestsComboBox";
+            this.labTestsComboBox.Size = new System.Drawing.Size(475, 45);
+            this.labTestsComboBox.TabIndex = 0;
+            this.labTestsComboBox.ValueMember = "LabTestCode";
             // 
             // chooseATestLabel
             // 
@@ -52,19 +58,25 @@ namespace SmartClinic.View
             this.chooseATestLabel.TabIndex = 1;
             this.chooseATestLabel.Text = "Choose a test:";
             // 
+            // labTestBindingSource
+            // 
+            this.labTestBindingSource.DataSource = typeof(SmartClinic.Model.LabTest);
+            // 
             // LabTestsForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1080, 600);
             this.Controls.Add(this.chooseATestLabel);
-            this.Controls.Add(this.testsComboBox);
+            this.Controls.Add(this.labTestsComboBox);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "LabTestsForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Lab Tests";
+            this.Load += new System.EventHandler(this.LabTestsForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.labTestBindingSource)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -72,7 +84,8 @@ namespace SmartClinic.View
 
         #endregion
 
-        private System.Windows.Forms.ComboBox testsComboBox;
+        private System.Windows.Forms.ComboBox labTestsComboBox;
         private System.Windows.Forms.Label chooseATestLabel;
+        private System.Windows.Forms.BindingSource labTestBindingSource;
     }
 }
