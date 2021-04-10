8b38e523a05ca21609fbaacc78a5a0d8167933e4
Mike Hamel <mhamel1@my.westga.edu>
2021-04-10 13:36:22 -0700

    Created LabTestResults model

---
+++ Model/LabTestResults.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SmartClinic.Model
+{
+    class LabTestResults
+    {
+        /// <summary>
+        /// AppointmentID for LabTestResults
+        /// </summary>
+        public int AppointmentID { get; set; }
+       
+        /// <summary>
+        /// LabTestCode for LabTestResults
+        /// </summary>
+        public string LabTestCode { get; set; }
+
+        /// <summary>
+        /// DatePerformed for LabTestResults
+        /// </summary>
+        public DateTime DatePerformed { get; set; }
+
+        /// <summary>
+        /// Result for LabTestResults
+        /// </summary>
+        public string Result { get; set; }
+
+        /// <summary>
+        /// IsNormal for LabTestResults
+        /// </summary>
+        public bool IsNormal { get; set; }
+
+    }
+}
