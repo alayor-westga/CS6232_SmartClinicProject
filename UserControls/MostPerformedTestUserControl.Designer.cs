e21da9ca6ddb7057c746cd5148f528ef3492275a
Mike Hamel <mhamel1@my.westga.edu>
2021-04-22 02:46:02 -0700

    added UI for user to enter date range and added code to run stored procedure with given dates

--- UserControls/MostPerformedTestUserControl.Designer.cs
+++ UserControls/MostPerformedTestUserControl.Designer.cs
@@ -30,20 +30,25 @@ namespace SmartClinic.UserControls
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource1 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource2 = new Microsoft.Reporting.WinForms.ReportDataSource();
             this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
             this._cs6232_g1DataSet = new SmartClinic._cs6232_g1DataSet();
             this.getMostPerformedTestsDuringDatesBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.getMostPerformedTestsDuringDatesTableAdapter = new SmartClinic._cs6232_g1DataSetTableAdapters.getMostPerformedTestsDuringDatesTableAdapter();
+            this.startDateLabel = new System.Windows.Forms.Label();
+            this.endDateLabel = new System.Windows.Forms.Label();
+            this.resultsButton = new System.Windows.Forms.Button();
+            this.startDateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.endDateTimePicker = new System.Windows.Forms.DateTimePicker();
             ((System.ComponentModel.ISupportInitialize)(this._cs6232_g1DataSet)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.getMostPerformedTestsDuringDatesBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // reportViewer1
             // 
-            reportDataSource1.Name = "MostPerformedTestsDataSet";
-            reportDataSource1.Value = this.getMostPerformedTestsDuringDatesBindingSource;
-            this.reportViewer1.LocalReport.DataSources.Add(reportDataSource1);
+            reportDataSource2.Name = "MostPerformedTestsDataSet";
+            reportDataSource2.Value = this.getMostPerformedTestsDuringDatesBindingSource;
+            this.reportViewer1.LocalReport.DataSources.Add(reportDataSource2);
             this.reportViewer1.LocalReport.ReportEmbeddedResource = "SmartClinic.MostPerformedTestsReport.rdlc";
             this.reportViewer1.Location = new System.Drawing.Point(21, 101);
             this.reportViewer1.Name = "reportViewer1";
@@ -65,10 +70,59 @@ namespace SmartClinic.UserControls
             // 
             this.getMostPerformedTestsDuringDatesTableAdapter.ClearBeforeFill = true;
             // 
+            // startDateLabel
+            // 
+            this.startDateLabel.AutoSize = true;
+            this.startDateLabel.Location = new System.Drawing.Point(26, 33);
+            this.startDateLabel.Name = "startDateLabel";
+            this.startDateLabel.Size = new System.Drawing.Size(72, 17);
+            this.startDateLabel.TabIndex = 1;
+            this.startDateLabel.Text = "Start Date";
+            // 
+            // endDateLabel
+            // 
+            this.endDateLabel.AutoSize = true;
+            this.endDateLabel.Location = new System.Drawing.Point(162, 33);
+            this.endDateLabel.Name = "endDateLabel";
+            this.endDateLabel.Size = new System.Drawing.Size(67, 17);
+            this.endDateLabel.TabIndex = 2;
+            this.endDateLabel.Text = "End Date";
+            // 
+            // resultsButton
+            // 
+            this.resultsButton.Location = new System.Drawing.Point(301, 52);
+            this.resultsButton.Name = "resultsButton";
+            this.resultsButton.Size = new System.Drawing.Size(141, 23);
+            this.resultsButton.TabIndex = 3;
+            this.resultsButton.Text = "Get Query Results";
+            this.resultsButton.UseVisualStyleBackColor = true;
+            this.resultsButton.Click += new System.EventHandler(this.resultsButton_Click);
+            // 
+            // startDateTimePicker
+            // 
+            this.startDateTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.startDateTimePicker.Location = new System.Drawing.Point(29, 53);
+            this.startDateTimePicker.Name = "startDateTimePicker";
+            this.startDateTimePicker.Size = new System.Drawing.Size(116, 22);
+            this.startDateTimePicker.TabIndex = 4;
+            // 
+            // endDateTimePicker
+            // 
+            this.endDateTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.endDateTimePicker.Location = new System.Drawing.Point(165, 53);
+            this.endDateTimePicker.Name = "endDateTimePicker";
+            this.endDateTimePicker.Size = new System.Drawing.Size(120, 22);
+            this.endDateTimePicker.TabIndex = 5;
+            // 
             // MostPerformedTestUserControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.endDateTimePicker);
+            this.Controls.Add(this.startDateTimePicker);
+            this.Controls.Add(this.resultsButton);
+            this.Controls.Add(this.endDateLabel);
+            this.Controls.Add(this.startDateLabel);
             this.Controls.Add(this.reportViewer1);
             this.Name = "MostPerformedTestUserControl";
             this.Size = new System.Drawing.Size(1086, 614);
@@ -76,6 +130,7 @@ namespace SmartClinic.UserControls
             ((System.ComponentModel.ISupportInitialize)(this._cs6232_g1DataSet)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.getMostPerformedTestsDuringDatesBindingSource)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -85,5 +140,10 @@ namespace SmartClinic.UserControls
         private _cs6232_g1DataSet _cs6232_g1DataSet;
         private System.Windows.Forms.BindingSource getMostPerformedTestsDuringDatesBindingSource;
         private _cs6232_g1DataSetTableAdapters.getMostPerformedTestsDuringDatesTableAdapter getMostPerformedTestsDuringDatesTableAdapter;
+        private System.Windows.Forms.Label startDateLabel;
+        private System.Windows.Forms.Label endDateLabel;
+        private System.Windows.Forms.Button resultsButton;
+        private System.Windows.Forms.DateTimePicker startDateTimePicker;
+        private System.Windows.Forms.DateTimePicker endDateTimePicker;
     }
 }
--- UserControls/MostPerformedTestUserControl.cs
+++ UserControls/MostPerformedTestUserControl.cs
@@ -20,9 +20,22 @@ namespace SmartClinic.UserControls
         private void MostPerformedTestsForm_Load(object sender, EventArgs e)
         {
 
-            this.getMostPerformedTestsDuringDatesTableAdapter.Fill(this._cs6232_g1DataSet.getMostPerformedTestsDuringDates, DateTime.Parse("1/1/2019"), DateTime.Parse("1/1/2022"));
 
-            this.reportViewer1.RefreshReport();
+
+           
+        }
+
+        private void resultsButton_Click(object sender, EventArgs e)
+        {
+            if (this.startDateTimePicker.Value < this.endDateTimePicker.Value)
+            {
+                this.getMostPerformedTestsDuringDatesTableAdapter.Fill(this._cs6232_g1DataSet.getMostPerformedTestsDuringDates, this.startDateTimePicker.Value, this.endDateTimePicker.Value);
+                this.reportViewer1.RefreshReport();
+            }
+            else
+            {
+                MessageBox.Show("Start Date Must be before End Date.", "Operation not allowed", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
         }
     }
 }
